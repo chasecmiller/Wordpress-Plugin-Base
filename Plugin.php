d225001676e1f51bc193cd3a1503e2ebc612a315
Chase C. Miller <chasecmiller@gmail.com>
2017-01-23 12:20:47 -0700

    Quick edit to header

--- Plugin.php
+++ Plugin.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: Plugin Name
-Plugin URI: https://github.com/RamEduard/skeleton-plugin-wordpress
+Plugin Name: Wordpress Plugin Base
+Plugin URI: https://github.com/chasecmiller/Wordpress-Plugin-Base
 Description: Description plugin
 Version: 1.0
 Author: Author Name
 Text Domain: crumbls-plugins-skeleton
-Domain Path: /languages/
+Domain Path: /Assets/Language/
 
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
